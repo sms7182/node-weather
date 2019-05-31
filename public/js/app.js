79c8b5b1c02bbe9d1c70a9254963638ac08ea532
sms7182@gmail.com <sms7182@gmail.com>
2019-05-31 16:47:15 +0430

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+console.log('css file loaded');
+
+fetch('http://localhost:7575/weatherApp?address=!').then((response)=>{
+    
+    console.log(response);
+   response.json().then((data)=>{
+   
+    if(data.error){
+           
+           console.log(data.error)
+       }
+       else{
+         console.log(data.location);
+         console.log(data.forecast);
+       }
+   })
+});
+
+const weatherForm=document.querySelector('form');
+const search=document.querySelector('input');
+
+const msg1=document.querySelector('#msg-1');
+const msg2=document.querySelector('#msg-2');
+
+//message.textContent='From JavaScript';
+weatherForm.addEventListener('submit',(e)=>{
+   e.preventDefault();
+
+   msg1.textContent='Loading ....';
+   fetch('http://localhost:7575/weatherApp?address='+search.value).then((response)=>{
+    
+    
+   response.json().then((data)=>{
+   
+    if(data.error){
+        msg1.textContent=data.error;
+        msg2.textContent="";       
+         
+       }
+       else{
+         msg1.textContent=data.location;
+         msg2.textContent=data.forecast;
+       }
+   })
+});
+})
---
+++ src/app.js
@@ -0,0 +1,107 @@
+const path=require('path');
+const express=require('express');
+const hbs=require('hbs');
+ const geocode=require('./utils/geocode');
+ const forecast=require('./utils/forecast');
+ 
+
+const app=express();
+const publicDirectory=path.join(__dirname,'../public');
+const viewsPath=path.join(__dirname,'../templates/views');
+const partialPath=path.join(__dirname,'../templates/partials')
+
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialPath);
+
+app.use(express.static(publicDirectory));
+ 
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather app',
+        name:'Mojtaba'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'Weather app',
+        name:'Mojtaba'
+    });
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText:'this help for you',
+        title:'Help',
+        name:'Mojtaba'
+    });
+})
+
+app.get('/weatherApp',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide address'
+        })
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+       if(error){
+           return res.send({error});
+       }
+       forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error});
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address,
+            })
+       });
+    })
+})
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+ return res.send({
+      error:'You must provide search term'
+  });
+  
+
+}
+return res.send({
+    forecast:'It is snowing',
+    location:'Philadelphia',
+    address:req.query.address
+});
+   
+});
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        name:'Mojtaba',
+        title:'404',
+        errorMessage:'Page not found'
+    })
+})
+
+
+app.get('/about/*',(req,res)=>{
+    res.render('404',{
+        name:'Mojtaba',
+        title:'404',
+        errorMessage:'Page not found'
+    })
+    
+})
+
+app.get('*',(req,res)=>{
+  res.render('404',{
+      name:'Mojtaba',
+      title:'404',
+      errorMessage:'Page not found'
+  })
+})
+
+app.listen(7575,()=>{
+    console.log('Server is up on port 7575');
+});
